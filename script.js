4590f09eab0d3fd1a7321b4e3146ed2b85d96118
dilshansethmina2002 <132134098+dilshansethmina2002@users.noreply.github.com>
2025-01-16 15:11:41 +0530

    Destination

---
+++ script.js
@@ -0,0 +1,67 @@
+function showMapGalle() {
+    const mapContainer = document.getElementById('mapContainer');
+    mapContainer.style.display = 'block';
+    mapContainer.innerHTML = `
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.123456789123!2d80.2218!3d6.0322!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae36b47fd78d4a9%3A0x7c1d16098c1c4515!2sGalle%20Fort!5e0!3m2!1sen!2slk!4v1674660000000!5m2!1sen!2slk"
+            width="100%"
+            height="100%"
+            style="border: 0;"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+    `;
+
+}
+
+function showMapSigiriya() {
+    const mapContainer = document.getElementById('mapContainer');
+    mapContainer.style.display = 'block';
+    mapContainer.innerHTML = `
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12672.658889737216!2d80.73829405391416!3d7.957479664510108!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3afca0f8c6d4c893%3A0x92e02418c33b799c!2sSigiriya!5e0!3m2!1sen!2slk!4v1674659893452!5m2!1sen!2slk"
+            width="100%"
+            height="100%"
+            style="border: 0;"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+    `;
+}
+
+function showMapArch() {
+    const mapContainer = document.getElementById('mapContainer');
+    mapContainer.style.display = 'block';
+    mapContainer.innerHTML = `
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3955.093456789123!2d81.04300000000001!3d6.876300000000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae465d1f1f1f1f1%3A0x2b2b2b2b2b2b2b2b!2sNine%20Arch%20Bridge!5e0!3m2!1sen!2slk!4v1674660000000!5m2!1sen!2slk"
+            width="100%"
+            height="100%"
+            style="border: 0;"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+    `;
+}
+
+
+function showMapNuwara() {
+    const mapContainer = document.getElementById('mapContainer');
+    mapContainer.style.display = 'block';
+    mapContainer.innerHTML = `
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.123456789123!2d80.76672!3d6.97177!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae465d1f1f1f1f1%3A0x2b2b2b2b2b2b2b2b!2sNuwara%20Eliya!5e0!3m2!1sen!2slk!4v1674660000000!5m2!1sen!2slk"
+            width="100%"
+            height="100%"
+            style="border: 0;"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+    `;
+}
+
+
